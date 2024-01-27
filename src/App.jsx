e53c1f657f3e8df350d4744aa5da658caa289bd3
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-01-27 20:52:54 -0300

    login & register design

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  let navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,41 +17,70 @@ export default function Login() {
       })
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
+    navigate("/home");
   };
   return (
-    <section>
-      <h1 className="text-center">LOGIN</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <div>
-            <label htmlFor="username">username</label>
-          </div>
-          <div>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
+    <section className="flex items-center justify-center h-screen">
+      <div className="w-9/12 mx-auto bg-white shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+        <div className="my-10">
+          <h1 className="text-black text-center mb-2 text-2xl font-bold">
+            PLAYLIST PARADISE
+          </h1>
+          <p className="text-black text-center">login here</p>
         </div>
-        <div>
-          <div>
-            <label htmlFor="password">password</label>
-          </div>
-          <div>
-            <input
-              type="text"
-              name="password"
-              id="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+        <div className="w-min mx-auto ">
+          <form onSubmit={handleSubmit}>
+            <div className="my-5">
+              <div className="mb-2">
+                <label className="text-black " htmlFor="username">
+                  USERNAME
+                </label>
+              </div>
+              <div>
+                <input
+                  className="border-2 border-black text-black rounded"
+                  type="text"
+                  name="username"
+                  id="username"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="my-5">
+              <div className="mb-2">
+                <label className="text-black " htmlFor="password">
+                  PASSWORD
+                </label>
+              </div>
+              <div>
+                <input
+                  className="border-2 border-black text-black rounded mb-5"
+                  type="text"
+                  name="password"
+                  id="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <button
+                className="text-white py-2 px-5 bg-black rounded mb-8"
+                type="submit"
+              >
+                LOGIN
+              </button>
+            </div>
+            <div className="text-center mb-5">
+              <p>
+                or register clicking{" "}
+                <Link className="underline" to="/register">
+                  here
+                </Link>
+              </p>
+            </div>
+          </form>
         </div>
-        <div>
-          <button type="submit">login</button>
-        </div>
-      </form>
+      </div>
     </section>
   );
 }
